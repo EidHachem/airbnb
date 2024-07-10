e15d86dd030492256f1b76ba98c2abfceadedaf6
EidHAchem <eid.hachem@sowlutions.com>
2024-07-10 10:47:05 +0300

    Add reviews table and relations

---
+++ app/models/review.rb
@@ -0,0 +1,19 @@
+class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :property
+
+    validates :content, presence: :true
+    validates :cleanliness_rating, numericality: {only_integer: true, grater_than_or_equal_to: 1, less_than_or_equal_to: 5} ,presence: :true
+    validates :accuracy_rating, numericality: {only_integer: true, grater_than_or_equal_to: 1, less_than_or_equal_to: 5} ,presence: :true
+    validates :checkin_rating, numericality: {only_integer: true, grater_than_or_equal_to: 1, less_than_or_equal_to: 5} ,presence: :true
+    validates :communication_rating, numericality: {only_integer: true, grater_than_or_equal_to: 1, less_than_or_equal_to: 5} ,presence: :true
+    validates :location_rating, numericality: {only_integer: true, grater_than_or_equal_to: 1, less_than_or_equal_to: 5} ,presence: :true
+    validates :value_rating, numericality: {only_integer: true, grater_than_or_equal_to: 1, less_than_or_equal_to: 5} ,presence: :true
+
+    after_commit :update_final_rating, on: [:create, :update]
+
+    def update_final_rating
+      average_rating = (cleanliness_rating + accuracy_rating + checkin_rating + communication_rating + location_rating + value_rating) / 6.0
+      update_column(:final_rating, average_rating)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+user = User.create!({
+  email: 'test@test1.com',
+  password: 123456,
+})
+
 100.times do |i|
   image_index_1 = (i % 12) + 1
   image_index_2 = ((i + 6) % 12) + 1
@@ -16,4 +21,18 @@
 
   property.images.attach(io: File.open("db/images/property_#{image_index_1}.png"), filename: "property_#{image_index_1}.png")
   property.images.attach(io: File.open("db/images/property_#{image_index_2}.png"), filename: "property_#{image_index_2}.png")
+
+  (5..10).to_a.sample.times do |j|
+    Review.create!({
+      content: Faker::Lorem.paragraph(sentence_count: 5),
+      cleanliness_rating: Faker::Number.between(from: 1, to: 5),
+      accuracy_rating: Faker::Number.between(from: 1, to: 5),
+      checkin_rating: Faker::Number.between(from: 1, to: 5),
+      communication_rating: Faker::Number.between(from: 1, to: 5),
+      location_rating: Faker::Number.between(from: 1, to: 5),
+      value_rating: Faker::Number.between(from: 1, to: 5),
+      property: property,
+      user: user
+    })
+  end
 end
