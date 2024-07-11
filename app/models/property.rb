505c66f3612b3e384b8a3ae95b774dfa5867e520
EidHAchem <eid.hachem@sowlutions.com>
2024-07-11 19:19:42 +0300

    Add reservations table

--- app/models/property.rb
+++ app/models/property.rb
@@ -14,6 +14,8 @@ class Property < ApplicationRecord
     has_many :reviews, dependent: :destroy
     has_many :wishlists, dependent: :destroy
     has_many :wishlisted_users, through: :wishlists, source: :user, dependent: :destroy
+    has_many :reservations, dependent: :destroy
+    has_many :reserved_users, through: :reservations, source: :user, dependent: :destroy
 
     def update_average_rating
         average_rating = reviews.average(:final_rating)
